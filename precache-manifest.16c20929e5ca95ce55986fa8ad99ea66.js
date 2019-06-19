7de1e525fd2eec1f045fbabb0090749017bd659a
Dan Spector <danno2423@gmail.com>
2019-06-19 18:53:06 -0400

    Updates

---
+++ precache-manifest.16c20929e5ca95ce55986fa8ad99ea66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de180438a2a02e15f3f13d3704a82dd3",
+    "url": "/every_second/index.html"
+  },
+  {
+    "revision": "e15ee10ada4b21f8cc21",
+    "url": "/every_second/static/css/main.84da4f39.chunk.css"
+  },
+  {
+    "revision": "063dff6d52078813ea22",
+    "url": "/every_second/static/js/2.1d89c3d1.chunk.js"
+  },
+  {
+    "revision": "e15ee10ada4b21f8cc21",
+    "url": "/every_second/static/js/main.3ebc3383.chunk.js"
+  },
+  {
+    "revision": "b130476d499993ccad31",
+    "url": "/every_second/static/js/runtime~main.f513a026.js"
+  }
+]);
